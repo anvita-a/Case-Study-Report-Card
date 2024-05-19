6a79afdaabe957b4d318af86f7f61ae2f0def732
anvita-a <143732520+anvita-a@users.noreply.github.com>
2024-05-19 21:54:07 +0530

    Update Makefile.mk

--- Makefile.mk
+++ Makefile.mk
@@ -1,18 +1,29 @@
-CC = gcc
+# Compiler
+CC = /usr/bin/gcc
+
+# Compiler flags
 CFLAGS = -Wall -Wextra -Werror
+
+# Target executable
 TARGET = case_study
+
+# Object files
 OBJS = case_study_mainfile.o functions.o
 
+# Default target
 $(TARGET): $(OBJS)
 	$(CC) $(CFLAGS) -o $(TARGET) $(OBJS)
 
+# Object file dependencies
 case_study_mainfile.o: case_study_mainfile.c student.h
 	$(CC) $(CFLAGS) -c case_study_mainfile.c
 
 functions.o: functions.c student.h
 	$(CC) $(CFLAGS) -c functions.c
 
+# Clean target
 clean:
 	rm -f $(TARGET) $(OBJS)
 
+# Phony targets
 .PHONY: clean
